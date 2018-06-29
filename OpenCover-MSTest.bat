87821905e056ca89bcf2ca423afb5f01c84ab633
vernon-naidoo <40565574+vernon-naidoo@users.noreply.github.com>
2018-06-29 17:39:31 +0200

    Update OpenCover-MSTest.bat

--- OpenCover-MSTest.bat
+++ OpenCover-MSTest.bat
@@ -4,7 +4,7 @@ REM OpenCover-MSTest.bat
 REM Run opencover against MSTest tests in your test project and show report of code coverage
 
 REM Build Variables
-SET BUILDMODE=Release
+SET BUILDMODE=DEBUG
 SET UNITTESTPROJECT=%WORKSPACE%\DSV.Webservice.Central.DMSRetrieval.Tests
 SET ASSEMBLIES=DSV.Webservice.Central.DMSRetrieval
 SET MSTEST_PATH=%PROGRAMFILES(X86)%\Microsoft Visual Studio 14.0\Common7\IDE
@@ -16,4 +16,4 @@ rd /S /Q TestResults
 md TestResults\HTMLJSReport
 
 REM 'Run the .NET Unit Tests with Coverage.'
-"C:\Tools\OpenCover\OpenCover.Console.exe" -target:"%PROGRAMFILES(X86)%\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" -targetargs:"/nologo /noisolation /testcontainer:%UNITTESTPROJECT%\bin\%BUILDMODE%\%UNITTESTPROJECT%.dll /resultsfile:TestResults\UnitTestReport.trx" -filter:"+[%ASSEMBLIES%*]*" -register -mergebyhash -hideskipped:Filter -output:"TestResults\OpenCoverCodeCoverage.xml"
+"C:\Tools\OpenCover\OpenCover.Console.exe" -target:"%MSTEST_PATH%\MSTest.exe" -targetargs:"/nologo /noisolation /testcontainer:%UNITTESTPROJECT%\bin\%BUILDMODE%\%UNITTESTPROJECT%.dll /resultsfile:TestResults\UnitTestReport.trx" -filter:"+[%ASSEMBLIES%*]*" -register -mergebyhash -hideskipped:Filter -output:"TestResults\OpenCoverCodeCoverage.xml"
