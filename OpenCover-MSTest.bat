16da3f0664832499460cc7c4f5936dc1f302720d
vernon-naidoo <40565574+vernon-naidoo@users.noreply.github.com>
2018-06-29 17:25:06 +0200

    Update OpenCover-MSTest.bat

--- OpenCover-MSTest.bat
+++ OpenCover-MSTest.bat
@@ -1,27 +1,3 @@
-@ECHO OFF
+@ECHO ON
 
-REM OpenCover-MSTest.bat
-
-REM Run opencover against MSTest tests in your test project and show report of code coverage
-
-REM Build Variables
-SET BUILDMODE=Release
-SET UNITTESTPROJECT=%WORKSPACE%\DSV.Webservice.Central.DMSRetrieval.Tests
-SET ASSEMBLIES=DSV.Webservice.Central.DMSRetrieval
-SET MSTEST_PATH=%PROGRAMFILES(X86)%\Microsoft Visual Studio 14.0\Common7\IDE
-
-REM 'Clean Unit Tests Reports'
-rd /S /Q TestResults
-md TestResults\HTMLJSReport
-
-REM 'Run the .NET Unit Tests with Coverage.'
-"C:\Tools\OpenCover\OpenCover.Console.exe" -target:"%PROGRAMFILES(X86)%\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" -targetargs:"/nologo /noisolation /testcontainer:%UNITTESTPROJECT%\bin\%BUILDMODE%\%UNITTESTPROJECT%.dll /resultsfile:TestResults\UnitTestReport.trx" -filter:"+[%ASSEMBLIES%*]*" -register -mergebyhash -hideskipped:Filter -output:"TestResults\OpenCoverCodeCoverage.xml"
-
-REM 'Convert MSTest report to JUnit report'
-"C:\Tools\MSTestToJUnit\msxsl.exe" TestResults\UnitTestReport.trx C:\Tools\MSTestToJUnit\mstest-to-junit.xsl -o TestResults\UnitTestsJUnitReport.xml
-
-REM 'Convert OpenConver report to Cobertura report'
-"C:\Tools\OpenCoverToCobertura\OpenCoverToCoberturaConverter.exe" -input:TestResults\OpenCoverCodeCoverage.xml -output:TestResults\CoberturaCodeCoverage.xml -sources:%WORKSPACE%
-
-REM 'Create HTML Report from OpenCover report.'
-"C:\Tools\ReportGenerator\ReportGenerator.exe" -reports:TestResults\CoberturaCodeCoverage.xml -targetDir:TestResults\HTMLCSReport
+echo Hello .... is it me you're looking for
