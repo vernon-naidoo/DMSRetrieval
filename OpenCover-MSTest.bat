d8365dfea90006046c89f027b2c647f1a3dc9e25
vernon-naidoo <40565574+vernon-naidoo@users.noreply.github.com>
2018-06-29 17:28:02 +0200

    Update OpenCover-MSTest.bat

--- OpenCover-MSTest.bat
+++ OpenCover-MSTest.bat
@@ -1,3 +1,15 @@
 @ECHO ON
 
-echo Hello .... is it me you're looking for
+REM OpenCover-MSTest.bat
+REM Run opencover against MSTest tests in your test project and show report of code coverage
+
+REM Build Variables
+SET BUILDMODE=Release
+SET UNITTESTPROJECT=%WORKSPACE%\DSV.Webservice.Central.DMSRetrieval.Tests
+SET ASSEMBLIES=DSV.Webservice.Central.DMSRetrieval
+SET MSTEST_PATH=%PROGRAMFILES(X86)%\Microsoft Visual Studio 14.0\Common7\IDE
+
+echo BUILDMODE: %BUILDMODE%
+echo UNITTESTPROJECT: %UNITTESTPROJECT%
+echo ASSEMBLIES: %ASSEMBLIES%
+echo MSTEST_PATH: %MSTEST_PATH%
