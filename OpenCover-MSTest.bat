aab8315c176c73e5e601658ee5649da6b50b4e7f
vernon-naidoo <40565574+vernon-naidoo@users.noreply.github.com>
2018-06-29 16:34:58 +0200

    replaced MSTEST_PATH

--- OpenCover-MSTest.bat
+++ OpenCover-MSTest.bat
@@ -15,7 +15,7 @@ rd /S /Q TestResults
 md TestResults\HTMLJSReport
 
 REM 'Run the .NET Unit Tests with Coverage.'
-"C:\Tools\OpenCover\OpenCover.Console.exe" -target:"%MSTEST_PATH%\MSTest.exe" -targetargs:"/nologo /noisolation /testcontainer:%UNITTESTPROJECT%\bin\%BUILDMODE%\%UNITTESTPROJECT%.dll /resultsfile:TestResults\UnitTestReport.trx" -filter:"+[%ASSEMBLIES%*]*" -register -mergebyhash -hideskipped:Filter -output:"TestResults\OpenCoverCodeCoverage.xml"
+"C:\Tools\OpenCover\OpenCover.Console.exe" -target:"%PROGRAMFILES(X86)%\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" -targetargs:"/nologo /noisolation /testcontainer:%UNITTESTPROJECT%\bin\%BUILDMODE%\%UNITTESTPROJECT%.dll /resultsfile:TestResults\UnitTestReport.trx" -filter:"+[%ASSEMBLIES%*]*" -register -mergebyhash -hideskipped:Filter -output:"TestResults\OpenCoverCodeCoverage.xml"
 
 REM 'Convert MSTest report to JUnit report'
 "C:\Tools\MSTestToJUnit\msxsl.exe" TestResults\UnitTestReport.trx C:\Tools\MSTestToJUnit\mstest-to-junit.xsl -o TestResults\UnitTestsJUnitReport.xml
